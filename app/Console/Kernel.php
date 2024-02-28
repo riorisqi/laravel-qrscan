5aaa7f4a20bcbd9e1e44441fd305ec48835013df
Rio Risqi Akbar Herlambang <rio.herlambang@bpddiy.co.id>
2024-02-28 09:24:19 +0700

    add scheduled task for deleting expired and unused qr files hourly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Storage;
+use Carbon\Carbon;
 
 class Kernel extends ConsoleKernel
 {
@@ -12,7 +14,22 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $directory = '/assets/img/qrcodeimg';
+            $disk = 'qrcodeimg';
+
+            $files = Storage::disk($disk)->allFiles($directory);
+            foreach ($files as $file) {
+                if(file_exists($file)){
+                    $time = Storage::disk($disk)->lastModified($file);
+                    $fileModifiedDateTime = Carbon::parse($time);
+                        
+                    if (Carbon::now()->gt($fileModifiedDateTime->addHour(1))) {
+                        Storage::disk($disk)->delete($file);
+                    }
+                }
+            }
+        })->hourly();
     }
 
     /**
