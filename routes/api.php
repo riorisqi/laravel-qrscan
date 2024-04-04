4ac08039ff37d2fbf5ffa8a55f3653ec92cd6b5c
Rio Risqi Akbar Herlambang <rio.herlambang@bpddiy.co.id>
2024-04-04 09:35:33 +0700

    remove unused files and folders, add ide helper

--- routes/api.php
+++ routes/api.php
@@ -5,22 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Authentication\AuthController;
 use App\Http\Controllers\QRLogin\QrLoginController;
 
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 // Authentication
 Route::prefix('auth')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
